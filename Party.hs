9c1b79aab5f9066e7153a6fdca43046fb2369d8b
xificurC <xificurC@gmail.com>
2014-08-17 23:21:53 +0200

    stuck with folds and exercise 4

--- Party.hs
+++ Party.hs
@@ -2,6 +2,7 @@ module Party where
 
 import Employee
 import Data.Monoid
+import Data.Tree
     
 glCons :: Employee -> GuestList -> GuestList
 glCons emp@(Emp _ fun) (GL emps funs) = GL (emp:emps) (fun+funs)
@@ -13,3 +14,25 @@ instance Monoid GuestList where
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun = max
+
+treeToList :: Tree a -> [a]
+treeToList (Node v s) = v : (concatMap treeToList s)
+                        
+-- treeFold :: (a -> b -> b) -> b -> Tree a -> b
+-- treeFold f z t = foldr f z (treeToList t)
+
+treeFold :: (a -> b -> b) -> Tree a -> b
+treeFold f (Node v s) = f v (concatMap (treeFold f) s)
+                             
+oneSubtree :: Employee -> (GuestList, GuestList) -> (GuestList, GuestList)
+oneSubtree emp@(Emp _ fun) (gl1, gl2@(GL emps2 funs2)) 
+    = ( (GL (emp:emps2) (fun+funs2)) , moreFun gl1 gl2)
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel emp gll = foldr1 (\(a1,b1) (a2,b2) -> (max a1 a2, max b1 b2))
+                    (zipWith oneSubtree (repeat emp) gll)
+
+-- maxFun :: Tree Employee -> GuestList
+-- maxFun [] = GL [] 0
+-- maxFun empTree = moreFun g1 g2
+--     where (g1, g2) = treeFold nextLevel [(GL [] 0, GL [] 0)] empTree
