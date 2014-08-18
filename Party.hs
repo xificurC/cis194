b01939657d9f6f087bce4dcf1c6f73b7d2bc3222
xificurC <xificurC@gmail.com>
2014-08-18 16:43:05 +0200

    correct treeFold, need maxFun

--- Party.hs
+++ Party.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeSynonymInstances #-}
 module Party where
 
 import Employee
@@ -15,15 +16,9 @@ instance Monoid GuestList where
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun = max
 
-treeToList :: Tree a -> [a]
-treeToList (Node v s) = v : (concatMap treeToList s)
-                        
--- treeFold :: (a -> b -> b) -> b -> Tree a -> b
--- treeFold f z t = foldr f z (treeToList t)
-
-treeFold :: (a -> b -> b) -> Tree a -> b
-treeFold f (Node v s) = f v (concatMap (treeFold f) s)
-                             
+treeFold :: Monoid b => (a -> b -> b) -> Tree a -> b
+treeFold f t = f (rootLabel t) (mconcat (map (treeFold f) (subForest t)))
+               
 oneSubtree :: Employee -> (GuestList, GuestList) -> (GuestList, GuestList)
 oneSubtree emp@(Emp _ fun) (gl1, gl2@(GL emps2 funs2)) 
     = ( (GL (emp:emps2) (fun+funs2)) , moreFun gl1 gl2)
@@ -31,8 +26,9 @@ oneSubtree emp@(Emp _ fun) (gl1, gl2@(GL emps2 funs2))
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
 nextLevel emp gll = foldr1 (\(a1,b1) (a2,b2) -> (max a1 a2, max b1 b2))
                     (zipWith oneSubtree (repeat emp) gll)
+                    
+-- foldForFun :: Employee -> [Employee] -> [Employee]
 
 -- maxFun :: Tree Employee -> GuestList
--- maxFun [] = GL [] 0
--- maxFun empTree = moreFun g1 g2
---     where (g1, g2) = treeFold nextLevel [(GL [] 0, GL [] 0)] empTree
+-- maxFun t = moreFun g1 g2
+--     where (g1,g2) = treeFold (nextLevel (rootLabel t)) (subForest t)
