712c72ec5cd4da097c1328f0caca98da58c8e38e
xificurC <xificurC@gmail.com>
2014-08-18 23:45:41 +0200

    maxFun done, writing main

--- Party.hs
+++ Party.hs
@@ -1,17 +1,17 @@
-{-# LANGUAGE TypeSynonymInstances #-}
 module Party where
 
 import Employee
 import Data.Monoid
 import Data.Tree
+import Text.Read (readMaybe)
     
 glCons :: Employee -> GuestList -> GuestList
 glCons emp@(Emp _ fun) (GL emps funs) = GL (emp:emps) (fun+funs)
 
 instance Monoid GuestList where
     mempty = GL [] 0
-    mappend (GL emps1 funs1) (GL emps2 funs2) = GL (emps1 ++ emps2)
-                                                (funs1 + funs2)
+    mappend (GL emps1 funs1) (GL emps2 funs2)
+        = GL (emps1 ++ emps2) (funs1 + funs2)
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun = max
@@ -21,14 +21,24 @@ treeFold f t = f (rootLabel t) (mconcat (map (treeFold f) (subForest t)))
                
 oneSubtree :: Employee -> (GuestList, GuestList) -> (GuestList, GuestList)
 oneSubtree emp@(Emp _ fun) (gl1, gl2@(GL emps2 funs2)) 
-    = ( (GL (emp:emps2) (fun+funs2)) , moreFun gl1 gl2)
+    = (GL (emp:emps2) (fun+funs2), moreFun gl1 gl2)
 
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
-nextLevel emp gll = foldr1 (\(a1,b1) (a2,b2) -> (max a1 a2, max b1 b2))
+nextLevel emp gll = foldr (\(a1,b1) (a2,b2) -> (max a1 a2, max b1 b2))
+                    (mempty, mempty)
                     (zipWith oneSubtree (repeat emp) gll)
                     
--- foldForFun :: Employee -> [Employee] -> [Employee]
+foldForFun :: Employee -> (GuestList, GuestList) -> (GuestList, GuestList)
+foldForFun emp glt = nextLevel emp [glt]
 
--- maxFun :: Tree Employee -> GuestList
--- maxFun t = moreFun g1 g2
---     where (g1,g2) = treeFold (nextLevel (rootLabel t)) (subForest t)
+
+maxFun :: Tree Employee -> GuestList
+maxFun t = moreFun g1 g2
+    where (g1,g2) = treeFold foldForFun t
+
+main :: IO ()
+main = do
+  s <- readFile "company.txt"
+  let t :: Tree Employee
+      t = read s
+  putStrLn (show t)
