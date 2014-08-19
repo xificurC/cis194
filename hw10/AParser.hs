60c90a168bb82eb8a85e0a6064560173875c816a
xificurC <xificurC@gmail.com>
2014-08-19 23:25:27 +0200

    Functor - done, Applicative - struggling

--- hw10/AParser.hs
+++ hw10/AParser.hs
@@ -57,3 +57,21 @@ posInt = Parser f
 ------------------------------------------------------------
 -- Your code goes below here
 ------------------------------------------------------------
+
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (a,c) = (f a, c)
+
+instance Functor Parser where
+    fmap f (Parser p) = Parser (fmap (first f) . p)
+
+instance Applicative Parser where
+    pure x = Parser (\s -> Just (x,s))
+    Parser p1 <*> Parser p2 =
+        (\x -> Parser $
+               case x of
+                 Nothing -> Nothing
+                 Just (f, s) -> case p2 s of
+                                  Nothing -> Nothing
+                                  Just (v, s2) -> Just (f v, s2))
+        . p1
+                              
