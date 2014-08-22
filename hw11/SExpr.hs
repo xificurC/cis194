5ea2e51332a81cb3e88a9d3cfe4e745f6a934996
xificurC <xificurC@gmail.com>
2014-08-22 17:13:39 +0200

    zeroOrMore and more done, working on SExpr

--- hw11/SExpr.hs
+++ hw11/SExpr.hs
@@ -6,16 +6,15 @@ module SExpr where
 
 import AParser
 import Control.Applicative
+import Data.Char
 
 ------------------------------------------------------------
 --  1. Parsing repetitions
 ------------------------------------------------------------
 
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+zeroOrMore p = oneOrMore p <|> pure []
                
-                 
-
 oneOrMore :: Parser a -> Parser [a]
 oneOrMore p = liftA2 (:) p (zeroOrMore p)
 
@@ -24,10 +23,15 @@ oneOrMore p = liftA2 (:) p (zeroOrMore p)
 ------------------------------------------------------------
 
 spaces :: Parser String
-spaces = undefined
+spaces = zeroOrMore (satisfy isSpace)
+
+infixr 5 <:>
+(<:>) :: Applicative f => f a -> f [a] -> f [a]
+(<:>) = liftA2 (:)
 
 ident :: Parser String
-ident = undefined
+-- ident = liftA2 (:) (satisfy isAlpha) (zeroOrMore (satisfy isAlphaNum))
+ident = satisfy isAlpha <:> zeroOrMore (satisfy isAlphaNum)
 
 ------------------------------------------------------------
 --  3. Parsing S-expressions
@@ -46,3 +50,21 @@ data Atom = N Integer | I Ident
 data SExpr = A Atom
            | Comb [SExpr]
   deriving Show
+
+getIdent :: Parser Ident
+getIdent = ident
+
+getN :: Parser Atom
+getN = N <$> posInt
+
+getI :: Parser Atom
+getI = I <$> getIdent
+       
+getAtom :: Parser Atom
+getAtom = getN <|> getI
+
+getA :: Parser SExpr
+getA = A <$> getAtom
+
+-- getComb :: Parser SExpr
+-- getComb = Comb <$> (char '(' *> getComb <* char ')')
