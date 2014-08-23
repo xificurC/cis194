597090a748935fe694335feff9a0dccffde4ebe0
xificurC <xificurC@gmail.com>
2014-08-23 13:55:03 +0200

    cleaned up some code

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -7,15 +7,15 @@ import Data.Char (intToDigit)
 -- 1. skips
 
 -- takeNth - given a list and an Int returns evey nth element (one-based)
-takeNth :: Int -> [a] -> [a]
-takeNth i a = case drop (i-1) a of
+takeNth :: [a] -> Int -> [a]
+takeNth a i = case drop (i-1) a of
                 [] -> []
-                (x:xs) -> x : takeNth i xs
+                (x:xs) -> x : takeNth xs i
 
 -- skips - given a list returns a list of lists 
--- that contain every [1..n]th element of the inital list
+-- that contains every [1..n]th element of the inital list
 skips :: [a] -> [[a]]
-skips lst = zipWith takeNth (repeat lst) [1..(length lst)]
+skips lst = map (takeNth lst) [1..(length lst)]
             
 
 -- 2. localMaxima
@@ -25,14 +25,14 @@ skips lst = zipWith takeNth (repeat lst) [1..(length lst)]
 -- e.g. part 3 1 [1..4] = [[1,2,3],[2,3,4]]
 part :: Int -> Int -> [a] -> [[a]]
 part _ _ [] = []
-part n _ lst | null $ drop (n-1) lst = []
+part n _ lst | null . drop (n-1) $ lst = []
 part n step lst = take n lst : part n step (drop step lst)
 
 -- localMaxima - given a list of integers returns all which
 -- are local maxima, i.e. both the left and right neighbors
 -- are strictly less than the element
 localMaxima :: [Integer] -> [Integer]
-localMaxima lst = foldr step [] $ part 3 1 lst
+localMaxima lst = foldr step [] (part 3 1 lst)
     where step :: [Integer] -> [Integer] -> [Integer]
           step [left,mid,right] acc =
               if mid > left && mid > right
@@ -52,6 +52,11 @@ buildString :: Int -> Int -> Int -> String
 buildString c m n = replicate (m-c) ' ' ++ replicate c '*' ++ ['=',intToDigit n]
                            
 histogram :: [Integer] -> String
-histogram lst = unlines $ transpose $ map (\x -> buildString (M.findWithDefault 0 x fMap) maxMap (fromInteger x)) [0..9]
+histogram lst = unlines . transpose $ map
+                (\x -> buildString
+                       (M.findWithDefault 0 x fMap)
+                       maxMap
+                       (fromInteger x))
+                [0..9]
     where fMap = frequencies lst
           maxMap = M.foldl' max 0 fMap
